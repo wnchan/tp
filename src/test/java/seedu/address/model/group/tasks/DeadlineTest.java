ed589dfb45565426c4e5cc8bd2de36ea493391c8
neyapraveen <neyapraveen@gmail.com>
2023-10-25 21:33:25 +0800

    Add test cases for some task classes

---
+++ src/test/java/seedu/address/model/group/tasks/DeadlineTest.java
@@ -0,0 +1,25 @@
+package seedu.address.model.group.tasks;
+
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class DeadlineTest {
+    void testToString() throws TaskException {
+        Deadline deadline = new Deadline("Finish assignment", false, "23/09/2023 1300");
+        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("MMM dd yyyy h:mm a");
+        String expectedFormattedDateTime = LocalDateTime.parse("23/09/2023 1300",
+                DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm")).format(outputFormatter);
+        String expectedToString = "[D][ ] Finish assignment (by: " + expectedFormattedDateTime + ")";
+        assertEquals(expectedToString, deadline.toString());
+    }
+
+    @Test
+    void testGenerateStr() throws TaskException {
+        Deadline deadline = new Deadline("Finish assignment", false, "23/09/2023 1300");
+        String expectedGenerateStr = "D | 0 | Finish assignment | 23/09/2023 1300";
+        assertEquals(expectedGenerateStr, deadline.generateStr());
+    }
+}
+
