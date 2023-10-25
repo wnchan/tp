2effccfd6332690cd3ca966d1ed4f4809a93ff90
neyapraveen <neyapraveen@gmail.com>
2023-10-25 13:20:56 +0800

    Remove unnecessary import

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -10,7 +10,6 @@ import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import seedu.address.model.person.Person;
-import seedu.address.model.socialmedialink.SocialMediaLink;
 
 /**
  * An UI component that displays information of a {@code Person}.
